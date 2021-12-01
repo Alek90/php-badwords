b2990b27e3dfbf73bb532b31717348168bfa5b17
Alek90 <4lek.90@gmail.com>
2021-12-01 16:24:49 +0100

    Create "badwars" in URL.

--- index.php
+++ index.php
@@ -4,7 +4,14 @@ $title = "Nuova Husqvarna Norden 901";
 
 $text = "La Norden 901 è una moto da Touring adventure eccezionale. Spinta da un fluido e potente bicilindrico parallelo da 889 cm³ da 105 CV alloggiato in un telaio a traliccio in acciaio, alla Norden 901 non manca nulla per i lunghi viaggi. Sospensioni pronte all’offroad con escursioni generose ti faranno superare qualsiasi terreno che troverai di fronte a te e l’ergonomia confortevole renderà ogni viaggio una piacevole avventura, non un percorso a ostacoli. Esplora il mondo come preferisci con la nuova Norden 901.";
 
+echo '<pre>
+
 var_dump($title, $text);
+
+var_dump($_GET("badwars"));
+</pre>';
+
+//var_dump($_GET("badwars"));
 
 ?>
 
