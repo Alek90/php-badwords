8b6577a3b3bdbea7f36134d94493fa7855370f13
Alek90 <4lek.90@gmail.com>
2021-12-01 17:51:46 +0100

    Finish.
    Censorship 2 in 1 in 2 values.  Lenght = (-1 letter)*3.

--- index.php
+++ index.php
@@ -4,9 +4,15 @@ $title = "Nuova Husqvarna Norden 901";
 
 $text = "La Norden 901 è una moto da Touring adventure eccezionale. Spinta da un fluido e potente bicilindrico parallelo da 889 cm³ da 105 CV alloggiato in un telaio a traliccio in acciaio, alla Norden 901 non manca nulla per i lunghi viaggi. Sospensioni pronte all’offroad con escursioni generose ti faranno superare qualsiasi terreno che troverai di fronte a te e l’ergonomia confortevole renderà ogni viaggio una piacevole avventura, non un percorso a ostacoli. Esplora il mondo come preferisci con la nuova Norden 901.";
 
-$model_name = $_GET["badwars"];
+$model_family = $_GET["badwars"];
 
 $model_sign = $_GET["badnumbers"];
+
+$model_name = [$model_family, $model_sign];
+
+$censure_text = str_replace($model_name, ["/---/", "***"], $text);
+
+var_dump($censure_text);
 
 echo '<pre>
 
@@ -28,15 +34,19 @@ var_dump($_GET("badwars"));
 </head>
 <body>
 
+
+    <!-- titolo -->
     <h1 class="title">
         <?php echo $title ?>
     </h1>
 
+    <!-- paragrafo -->
     <p class="content">
         <b>Presentazione:</b>
         <?php echo $text ?>
     </p>
 
+    <!-- dati paragrafo -->
     <div class="text_lenght">
         <b>Numero di caratteri del testo:</b>
         <?php echo mb_strlen($text) ?>
@@ -44,16 +54,19 @@ var_dump($_GET("badwars"));
         <?php echo strlen($text) ?>
     </div>
 
+
+    <!-- paragrafo censurato -->
     <p class="censure_content">
         <b>Anteprima testo:</b>
-        <?php echo str_replace([$model_name, $model_sign], ["***"], $text) ?>
+        <?php echo $censure_text ?>
     </p>
 
+    <!-- dati paragrafo censurato -->
     <div class="censure_lenght">
         <b>Numero di caratteri del testo:</b>
-        <?php echo mb_strlen($text) ?>
+        <?php echo mb_strlen($censure_text) ?>
         /
-        <?php echo strlen($text) ?>
+        <?php echo strlen($censure_text) ?>
     </div>
     
     
