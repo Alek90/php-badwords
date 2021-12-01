c15cad094b1d802bd104a65db6f9c510da9897f8
Alek90 <4lek.90@gmail.com>
2021-12-01 15:24:09 +0100

    Start exercise.
    Create and clone repo.  Create and print $text.

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+$text = "MV Agusta presenta Lucky Explorer Project 9.5, l'erede della mitica Cagiva Elefant.";
+
+var_dump($text);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <p>
+        Articolo <?php echo $text ?>
+    </p>
+    
+</body>
+</html>
